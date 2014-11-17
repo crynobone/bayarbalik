1be19b705d408a676a273e0d95a6a2cee4e9ac44
Zulfa Juniadi Zulkifli <zulfajuniadi@gmail.com>
2014-11-17 22:22:51 +0800

    Renamed view hello.php to hello.blade.php to text out elixir helper

---
+++ resources/views/hello.blade.php
@@ -0,0 +1,12 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>BayarBalik.com</title>
+	<link rel="stylesheet" href="{{ elixir("css/all.css") }}">
+</head>
+<body>
+	<h1>Hello, there</h1>
+	<script src="{{ elixir("js/all.js") }}"></script>
+</body>
+</html>
